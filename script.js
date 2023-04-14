090abb187a4c5b715b068ec420ce20f31657b2c3
rehanakabirmim <rehanakabirmim@gmail.com>
2023-04-14 20:37:08 +0600

    marksheet using js

---
+++ script.js
@@ -0,0 +1,433 @@
+
+     const result1 = () =>{
+        let tMark = document.querySelector(".tMark1").value;
+        let pMark = document.querySelector(".pMark1").value;
+        let grade = document.querySelector(".grade1");
+        let point = document.querySelector(".point1");
+        let totalMark = document.querySelector(".totalMark1");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value = 0.0;
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+
+    const result2 = () =>{
+        let tMark = document.querySelector(".tMark2").value;
+        let pMark = document.querySelector(".pMark2").value;
+        let grade = document.querySelector(".grade2");
+        let point = document.querySelector(".point2");
+        let totalMark = document.querySelector(".totalMark2");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0;
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+
+    const result3 = () =>{
+        let tMark = document.querySelector(".tMark3").value;
+        let pMark = document.querySelector(".pMark3").value;
+        let grade = document.querySelector(".grade3");
+        let point = document.querySelector(".point3");
+        let totalMark = document.querySelector(".totalMark3");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+    const result4 = () =>{
+        let tMark = document.querySelector(".tMark4").value;
+        let pMark = document.querySelector(".pMark4").value;
+        let grade = document.querySelector(".grade4");
+        let point = document.querySelector(".point4");
+        let totalMark = document.querySelector(".totalMark4");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+    const result5 = () =>{
+        let tMark = document.querySelector(".tMark5").value;
+        let pMark = document.querySelector(".pMark5").value;
+        let grade = document.querySelector(".grade5");
+        let point = document.querySelector(".point5");
+        let totalMark = document.querySelector(".totalMark5");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+    const result6 = () =>{
+        let tMark = document.querySelector(".tMark0").value;
+        let pMark = document.querySelector(".pMark0").value;
+        let grade = document.querySelector(".grade0");
+        let point = document.querySelector(".point0");
+        let totalMark = document.querySelector(".totalMark0");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+
+    
+    const result7 = () =>{
+        let tMark = document.querySelector(".tMark7").value;
+        let pMark = document.querySelector(".pMark7").value;
+        let grade = document.querySelector(".grade7");
+        let point = document.querySelector(".point7");
+        let totalMark = document.querySelector(".totalMark7");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+
+    
+    const result8 = () =>{
+        let tMark = document.querySelector(".tMark8").value;
+        let pMark = document.querySelector(".pMark8").value;
+        let grade = document.querySelector(".grade8");
+        let point = document.querySelector(".point8");
+        let totalMark = document.querySelector(".totalMark8");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+    const result9 = () =>{
+        let tMark = document.querySelector(".tMark9").value;
+        let pMark = document.querySelector(".pMark9").value;
+        let grade = document.querySelector(".grade9");
+        let point = document.querySelector(".point9");
+        let totalMark = document.querySelector(".totalMark9");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+    const result10 = () =>{
+        let tMark = document.querySelector(".tMark10").value;
+        let pMark = document.querySelector(".pMark10").value;
+        let grade = document.querySelector(".grade10");
+        let point = document.querySelector(".point10");
+        let totalMark = document.querySelector(".totalMark10");
+        
+        let total = Number(tMark) + Number(pMark);
+        totalMark.value = total;
+        if( total >= 80 && total <= 100){
+            grade.value = "A+";
+            point.value = 5.0;
+        }
+        else if(total >= 70 && total <= 79){
+            grade.value = "A";
+            point.value = 4.0;
+        }
+        else if(total >= 60 && total <= 69){
+            grade.value = "A-";
+            point.value = 3.5;
+        }
+        else if(total >= 50 && total <= 59){
+            grade.value = "B";
+            point.value = 3.0;
+        }
+        else if(total >= 40 && total <= 49){
+            grade.value = "C";
+            point.value = 2.0;
+        }
+        else if(total >= 33 && total <= 39){
+            grade.value = "D";
+            point.value = 1.0;
+        }
+        else if(total >= 0 && total <= 32){
+            grade.value = "F";
+            point.value=0.0
+        }
+        else{
+            alert("Invalid Input");
+        }
+
+    }
+
+    
+   
+    
+   
+    
